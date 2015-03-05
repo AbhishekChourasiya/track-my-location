a09abdd90ba2e06c863719e33a4770112eb77615
michaeltys <michael.tys8@gmail.com>
2015-03-05 16:52:38 +0200

    some unit tests

--- LocationAppAndroid/app/src/androidTest/java/com/techmagic/locationapp/ApiTest.java
+++ LocationAppAndroid/app/src/androidTest/java/com/techmagic/locationapp/ApiTest.java
@@ -13,13 +13,41 @@ import java.util.List;
 
 public class ApiTest extends InstrumentationTestCase {
 
+    private String deviceId = "unitTestDeviceId";
+
     public void testAddTrack() {
         ITrackLocationClient client = new TrackLocationClient();
 
         TrackLocationRequest request = new TrackLocationRequest();
-        request.setDeviceId("testDeviceId");
+        request.setDeviceId(deviceId);
         List<LatLonTime> locations = new ArrayList<LatLonTime>();
         locations.add(LatLonTime.getInstance(0, 0, System.currentTimeMillis()));
+        request.setLocations(locations);
+
+        TrackLocationResponse response = client.addTrack(request);
+        assertNotNull(response);
+        assertEquals(response.getStatus(), TrackLocationResponse.RESPONSE_CODE_OK);
+    }
+
+    public void testAddEmptyTrackArray() {
+        ITrackLocationClient client = new TrackLocationClient();
+
+        TrackLocationRequest request = new TrackLocationRequest();
+        request.setDeviceId(deviceId);
+        List<LatLonTime> locations = new ArrayList<LatLonTime>();
+        request.setLocations(locations);
+
+        TrackLocationResponse response = client.addTrack(request);
+        assertNotNull(response);
+        assertEquals(response.getStatus(), TrackLocationResponse.RESPONSE_CODE_OK);
+    }
+
+    public void testAddTrackNoTrackProperty() {
+        ITrackLocationClient client = new TrackLocationClient();
+
+        TrackLocationRequest request = new TrackLocationRequest();
+        request.setDeviceId(deviceId);
+        List<LatLonTime> locations = new ArrayList<LatLonTime>();
         request.setLocations(locations);
 
         TrackLocationResponse response = client.addTrack(request);
